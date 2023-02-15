9c1c79d030d5756484734d76c2ca149546176f34
Alissa Sheikh <89821902+asheikh3378@users.noreply.github.com>
2023-02-15 14:55:42 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# Alissa Sheikh 1623952
+
+# Integer input/presentation
+print("Enter integer:")
+user_num = int(input())
+print("You entered:", user_num)
+
+# mathematical problem
+square = user_num ** 2
+# Solution presentation
+print(f'{user_num} squared is {square}')
+
+# mathematical problem
+cube = user_num ** 3
+
+# Solution presentation
+print(f'And {user_num} cubed is {cube} !!')
+
+# Second input/presentation
+print("Enter another integer:")
+second_num = int(input())
+
+# mathematical problems
+sum = user_num + second_num
+product = user_num * second_num
+
+# Solution presentation
+print(f"{user_num} + {second_num} is {sum}")
+print(f"{user_num} * {second_num} is {product}")
