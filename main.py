36649e8a3047abe3ac361d96a4ec29d1b24f56f8
Alissa Sheikh <89821902+asheikh3378@users.noreply.github.com>
2023-02-15 14:29:14 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# Alissa Sheikh 1623952
+
+# Bug here; fix it when instructed
+userNum = int(input())
+userNumSquared = userNum * userNum
+
+# Output formatting issue here; fix it when instructed
+print(userNumSquared, end='\n')
